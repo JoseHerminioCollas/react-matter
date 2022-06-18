ba389a135a427fb38fa590c83e89813b95f802db
Jose Collas <joseherminiocollas@goatstone.com>
2022-06-18 18:52:59 -0500

    add throttle to tick

--- src/matterMotor.ts
+++ src/matterMotor.ts
@@ -1,17 +1,20 @@
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, interval } from 'rxjs';
+import { throttle } from 'rxjs/operators';
 
 const strm$ = new BehaviorSubject([0, 0]);
 export { strm$ };
 
 const matterMotor = {
-    emit: ( id: number, coords: number[]) => {
-        strm$.next(coords)
-    },
-    listen: (cb: any) => {
-        strm$.subscribe(coords => {
-            cb(coords)
-        })
-    }
- }
+  emit: (id: number, coords: number[]) => {
+    strm$.next(coords)
+  },
+  listen: (cb: any) => {
+    strm$
+      .pipe(throttle(val => interval(500)))
+      .subscribe(coords => {
+        cb(coords)
+      })
+  }
+}
 
 export default matterMotor;
